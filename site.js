7ff55f3f9a50b7b7b820d40ce3449ae1f67b35c8
KrystianKunowski <krystian.kunowski.home@gmail.com>
2020-11-28 21:56:34 +0000

    2nd commit

--- site.js
+++ site.js
@@ -4,12 +4,16 @@ function bmi() {
     if (num1 <= 0 || Number.isNaN(num1)) {
       var text1 = " Wrong Height Input " + "<br>check everything one more time, will ya?";
       document.getElementById('messagePanel').innerHTML = text1;
+      document.getElementById('height').value = "";
+      document.getElementById('weight').value = "";
       return;
     }
     var num2 = +document.getElementById('weight').value;
     if (num2 <= 0 || Number.isNaN(num2)) {
       var text2 = " Wrong Weight Input " + "<br>check everything one more time, will ya?";
       document.getElementById('messagePanel').innerHTML = text2;
+      document.getElementById('height').value = "";
+      document.getElementById('weight').value = "";
       return;
     }
     var num1 = num1 / 100;
@@ -18,22 +22,32 @@ function bmi() {
 
     if ( fixedSum < 18.5){
     document.getElementById('messagePanel').innerHTML = fixedSum + "<br>Underweight";
+    document.getElementById('height').value = "";
+    document.getElementById('weight').value = "";
     return;
     }
     if ( fixedSum >= 18.5 && fixedSum <= 24.9){
       document.getElementById('messagePanel').innerHTML = fixedSum + "<br>Normal";
+      document.getElementById('height').value = "";
+      document.getElementById('weight').value = "";
     return;
     }
     if ( fixedSum >= 25.0 && fixedSum <= 29.0){
         document.getElementById('messagePanel').innerHTML = fixedSum + "<br>Overweight";
+        document.getElementById('height').value = "";
+        document.getElementById('weight').value = "";
     return;
     }
     if ( fixedSum >= 30.0 && fixedSum <= 40.0){
           document.getElementById('messagePanel').innerHTML = fixedSum + "<br>Obese";
+          document.getElementById('height').value = "";
+          document.getElementById('weight').value = "";
     return;
     }
     if ( fixedSum > 40.0){
           document.getElementById('messagePanel').innerHTML = fixedSum + "<br>Extreme Obese";
+          document.getElementById('height').value = "";
+          document.getElementById('weight').value = "";
     return;
     }
   }
